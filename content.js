c795f2c63c17134a0c3ab6e74f2f20c51bd52f8c
youcanreadextension <103051199+youcanreadextension@users.noreply.github.com>
2022-05-08 20:36:45 +0300

    0.1.1.1

--- content.js
+++ content.js
@@ -1,7 +1,60 @@
+//"created by David Tantz david.tantz@gmail.com do not copy",
 
 var container1 ="";
 let textColorRotater = 0;
-window.addEventListener('mouseup', mousedUped);
+var htmlTagSplitter = /(?<script><script>.+>)|(?<htmlTag><[^>]*>)|(?<htmlCharacter>&[^>]*;)|(?<word>\b\w+\b)|(?<other>.)|/gm;
+var reHTMLtag = /(?<htmlTag><[^>]*>)/;
+var reHhtmlCharacter = /(?<htmlCharacter>&[^>]*;)/;
+var reword = /(?<word>\b\w+\b)/;
+var rother = /(?<other>.)/;
+var rStartLink = /(?<other>href=)/;
+var rEndLink = /(?<endLink><\/a>)/;
+let linkText = false;
+
+
+let paragraphs = document.getElementsByTagName('p');
+for (elt of paragraphs) {
+    if (elt.innerHTML  != "\n" ){
+    elt.innerHTML = splitedTextRender (elt.innerHTML) ;
+    }
+}
+let paragraphsH2 = document.getElementsByTagName('h2');
+for (elt of paragraphsH2) {
+    if (elt.innerHTML  != "\n" ){
+    elt.innerHTML = splitedTextRender (elt.innerHTML) ;
+    }
+}
+let paragraphsH3 = document.getElementsByTagName('h3');
+for (elt of paragraphsH3) {
+    if (elt.innerHTML  != "\n" ){
+    elt.innerHTML = splitedTextRender (elt.innerHTML) ;
+    }
+}
+let paragraphsH4 = document.getElementsByTagName('h4');
+for (elt of paragraphsH4) {
+    if (elt.innerHTML  != "\n" ){
+    elt.innerHTML = splitedTextRender (elt.innerHTML) ;
+    }
+}
+let paragraphsH5 = document.getElementsByTagName('h5');
+for (elt of paragraphsH5) {
+    if (elt.innerHTML  != "\n" ){
+    elt.innerHTML = splitedTextRender (elt.innerHTML) ;
+    }
+}
+let paragraphsH6 = document.getElementsByTagName('h6');
+for (elt of paragraphsH6) {
+    if (elt.innerHTML  != "\n" ){
+    elt.innerHTML = splitedTextRender (elt.innerHTML) ;
+    }
+}
+let paragraphsH1 = document.getElementsByTagName('h1');
+for (elt of paragraphsH1) {
+    if (elt.innerHTML  != "\n" ){
+    elt.innerHTML = splitedTextRender (elt.innerHTML) ;
+    }
+}
+//window.addEventListener('mouseup', mousedUped);
 function mousedUped() {
     container1 ="";
     var text = "", containerElement = null;
@@ -18,9 +71,12 @@ function mousedUped() {
         containerElement = textRange.parentElement();
         text = textRange.text;
     }
-  let selectedText = containerElement.innerHTML;
-  selectedText.replace (htmlTagSplitter,splitedTextRender);  
-  containerElement.innerHTML = container1;
+    let selectedText = containerElement.innerHTML;
+    var inputBox =/<[^>]*?input/.test(selectedText);
+    if (inputBox == false) {
+        var rendordText = splitedTextRender (selectedText);
+        containerElement.innerHTML = rendordText;
+        }
 }
 function getSyllables(word){
     var response = [];
@@ -53,12 +109,12 @@ function getSyllables(word){
                 response.push(word);
                 isSpecialCase = true;
                 }
-  
+
             }                    
         }
     }
     if (isSpecialCase == false){
-        const syllableRegex = /[^aeiouy]*[aeiouy]+(?:[^aeiouy]*$|[^aeiouy](?=[^aeiouy]))?/g;
+        const syllableRegex = /[^aeiouy]*[aeiouy\d]+(?:[^aeiouy]*$|[^aeiouy](?=[^aeiouy]))?|/gi;
         response = word.match(syllableRegex);
     }
     return response;
@@ -78,29 +134,55 @@ function getSyllables(word){
   function hasRecurringConsonant(word){
     return / ([b-df-hj-np-tv-z])\1{1,}/.test(word);
   }
-var htmlTagSplitter = /(?<htmlTag><[^>]*>)|(?<word>\b\w+\b)|(?<other>.)|/gm;
 function splitedTextRender(match) {
+    container1 ="";
+    textColorRotater = 0;
+    var pizza ;
    // console.log(match);
-    var reHTMLtag = /(?<htmlTag><[^>]*>)/;
-    var reword = /(?<word>\b\w+\b)/;
-    if (reHTMLtag.test(match) == true) {
-        container1 = container1 + (match);  
-    } else if (reword.test(match) == true) {
-        var container = document.createElement("span");
-        match = getSyllables(match);
-        match.forEach((textItem, i) => {
-        var span = document.createElement("span");
-          if (textColorRotater % 2 == 0) {
-              span.style.color = "black";
-          } else {
-              span.style.color = "#696969";
-          }
-          textColorRotater ++;
-          span.innerHTML = textItem;
-          container.appendChild(span);  
-        })
-        container1 = container1 + (container.innerHTML); 
-} else {
-    container1 = container1 + (match);
-}
+    pizza = match.match(htmlTagSplitter);
+    pizza.forEach (match => {
+        //console.log(match);
+        if (reHTMLtag.test(match) == true || reHhtmlCharacter.test(match) == true) {
+                    if (rStartLink.test(match) == true) {
+                        linkText = true;                
+                    } else {
+                        linkText = false; 
+                    }
+                container1 = container1 + (match);  
+            } 
+            else if (reword.test(match) == true) {
+                var container = document.createElement("span");
+                syllablArray = getSyllables(match);
+                syllablArray.forEach((textItem, i) => {
+                 //   console.log(textItem);
+                var span = document.createElement("span");
+                    if (linkText == false) {
+                        if (textColorRotater % 2 == 0) {
+                            span.style.color = "black";
+                        } else {
+                            span.style.color = "#696969";
+                        }
+                        textColorRotater ++;
+                    } else {
+                        if (textColorRotater % 2 == 0) {
+                            span.style.color = "#000099";
+                        } else {
+                            span.style.color = "#0066ff";
+                        }
+                        textColorRotater ++;
+        
+                    }
+                span.innerHTML = textItem;
+                container.appendChild(span);  
+                })
+                container1 = container1 + (container.innerHTML); 
+            } else if (rother.test(match) == true) {
+                container1 = container1 + (match);
+            }
+            else{
+                container1 = container1 + (match);
+            }
+    })
+   // console.log(container1);
+    return container1;
 }
